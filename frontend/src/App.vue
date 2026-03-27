83a96e2b758f709925524057350a266058ad9631
Plean <plean_som@cmu.ac.th>
2026-03-27 14:36:29 +0700

    setup environment

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { fetchTest } from './services/api';
+
+const message = ref('');
+
+onMounted(async () => {
+  const data = await fetchTest();
+  message.value = data.message;
+});
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
